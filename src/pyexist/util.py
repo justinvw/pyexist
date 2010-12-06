d5f69fb922b3160821e4c954da02b104d8e5fa56
Samuel Abels <knipknap@gmail.com>
2010-12-06 17:41:45 +0100

    replacetags() now supports list variables.

--- src/pyexist/util.py
+++ src/pyexist/util.py
@@ -5,6 +5,9 @@ class safe(str):
 def escape(arg):
     if isinstance(arg, safe):
         return arg
+    if hasattr(arg, '__iter__'):
+        items = [("'" + escape(i) + "'") for i in arg]
+        return ', '.join(items)
     return str(arg).replace(r"'", r"''")
 
 def replacetags(string, **kwargs):
